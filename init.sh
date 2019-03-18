fc635a284ae2ead72ce4c84986479377714f29ac
aryannarora <aryanarora278@gmail.com>
2019-03-19 03:48:00 +0530

    m

---
+++ init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#int_ip=$(hostname -i)
+#ext_ip=$(curl ifconfig.me)
+
+#sed -i "s/listening-ip=.*/listening-ip=${int_ip}/" turnserver.conf
+#sed -i "s/relay-ip=.*/relay-ip=${ext_ip}/" turnserver.conf
+#sed -i "s/external-ip=.*/external-ip=${ext_ip}\/${int_ip}/" turnserver.conf
+
+#sudo cp turnserver.conf /usr/local/etc/turnserver.conf
+
+echo "enter mysql host (Default: localhost)"
+read host
+
+echo "enter db name (Default: coturn)"
+read db
+
+echo "enter username (Default: user)"
+read user
+
+echo "enter password: "
+read password
+
+sudo sed -i "s/host=.*/host=${host}/" /usr/local/etc/turnserver.conf
+sudo sed -i "s/db=.*/db=${db}/" /usr/local/etc/turnserver.conf
+sudo sed -i "s/user=.*/user=${user}/" /usr/local/etc/turnserver.conf
+sudo sed -i "s/password=.*/password=${password}/" /usr/local/etc/turnserver.conf
